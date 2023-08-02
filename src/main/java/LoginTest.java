a55bf3080889ca520499ede7ef794f8caf5a5e6a
tabita <73694935+randunica18@users.noreply.github.com>
2023-08-02 14:06:50 +0300

    RegisterTest - DONE

--- src/main/java/LoginTest.java
+++ src/main/java/LoginTest.java
@@ -3,20 +3,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
+    String greenColorCode = "\u001B[32m";
+    String resetColorCode = "\u001B[0m";
 
     public void validLoginTest(){
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://qa1magento.dev.evozon.com/");
+        driver.get("http://qa3magento.dev.evozon.com/");
         driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
         driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
-        driver.findElement(By.id("email")).sendKeys("cosmin@evo.com");
-        driver.findElement(By.id("pass")).sendKeys("123123");
+        driver.findElement(By.id("email")).sendKeys("tabylucaciu@yahoo.com");
+        driver.findElement(By.id("pass")).sendKeys("password");
         driver.findElement(By.id("send2")).click();
         String welcomeText = driver.findElement(By.cssSelector("body > div > div.page > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div > div.welcome-msg > p.hello > strong")).getText();
 
-        if (welcomeText.equalsIgnoreCase("Hello, aaa dasda!"))
-            System.out.println("Success!");
+        if (welcomeText.equalsIgnoreCase("Hello, Tabita Semida Lucaciu!"))
+            System.out.println(greenColorCode + "Success!" + resetColorCode);
         else
             System.err.println("!Success");
 
---
+++ src/main/java/RegisterTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RegisterTest {
+    String greenColorCode = "\u001B[32m";
+    String resetColorCode = "\u001B[0m";
+
+    public void validRegisterTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://qa3magento.dev.evozon.com/");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Tabita");
+        driver.findElement(By.id("lastname")).sendKeys("Lucaciu");
+        driver.findElement(By.id("email_address")).sendKeys("tabylucaciu2@yahoo.com");
+        // increment the value after "tabylucaciu" with 1 everytime
+        driver.findElement(By.id("password")).sendKeys("password");
+        driver.findElement(By.id("confirmation")).sendKeys("password");
+        driver.findElement(By.id("is_subscribed")).click();
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
+        String welcomeText = driver.findElement(By.cssSelector("body > div > div > div.main-container.col2-left-layout > div > div.col-main > div.my-account > div > div.welcome-msg > p.hello > strong")).getText();
+
+        if (welcomeText.equalsIgnoreCase("Hello, Tabita Lucaciu!"))
+            System.out.println(greenColorCode + "Successfully registered!" + resetColorCode);
+        else
+            System.err.println("NOT REGISTERED");
+
+        driver.close();
+    }
+}
